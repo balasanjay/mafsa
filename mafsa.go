2c7f5e513a4acc172e9c26c971ae59e9a7cdc53c
Sanjay Menakuru <balasanjay@gmail.com>
2012-09-22 00:49:18 -0700

    Added a working trie implementation.

--- mafsa.go
+++ mafsa.go
@@ -1,9 +1,83 @@
 package mafsa
 
-import (
-	"fmt"
-)
+type node struct {
+	first, last *node // linked list of children
+	next        *node
 
-func main() {
-	fmt.Println("Hello, world")
+	isWord bool
+
+	b byte
+}
+
+type builder struct {
+	root *node
+}
+
+func (b *builder) InsertWord(s string) {
+	if b.root == nil {
+		b.root = &node{}
+	}
+
+	current := b.root
+	i := 0
+	for ; i < len(s); i++ {
+		b := s[i]
+
+		if current.first == nil {
+			break
+		}
+
+		if current.last.b < b {
+			break
+		}
+
+		current = current.last
+	}
+
+	if i == len(s) {
+		current.isWord = true
+		return
+	}
+
+	for ; i < len(s); i++ {
+		n := &node{b: s[i]}
+
+		if current.last == nil {
+			current.first = n
+			current.last = n
+			current = n
+		} else {
+			tmp := current.last
+			current.last = n
+			tmp.next = n
+			current = n
+		}
+	}
+
+	current.isWord = true
+}
+
+func (b *builder) Contains(s string) bool {
+	if b.root == nil {
+		return false
+	}
+
+	current := b.root
+
+Outer:
+	for len(s) > 0 {
+		b := s[0]
+		s = s[1:]
+
+		for cand := current.first; cand != nil; cand = cand.next {
+			if cand.b == b {
+				current = cand
+				continue Outer
+			}
+		}
+
+		return false
+	}
+
+	return current.isWord
 }
--- mafsa_test.go
+++ mafsa_test.go
@@ -1,7 +1,23 @@
-package mafsa_test
+package mafsa
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestEmpty(t *testing.T) {}
+func TestEmpty(t *testing.T) {
+	b := &builder{}
+	b.InsertWord("abc")
+	b.InsertWord("abcde")
+	b.InsertWord("abcd")
+	b.InsertWord("cde")
+
+	fmt.Println(b.Contains("ab"))
+	fmt.Println(b.Contains("abc"))
+	fmt.Println(b.Contains("abe"))
+	fmt.Println(b.Contains("abcde"))
+	fmt.Println(b.Contains("abd"))
+	fmt.Println(b.Contains("abe"))
+	fmt.Println(b.Contains("cdef"))
+	fmt.Println(b.Contains("cde"))
+}
